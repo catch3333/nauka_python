34b91c21a0403e743abe2ad3237cc2cbc1689109
catch3333 <piotrb.jureczka@gmail.com>
2021-05-09 11:43:15 +0000

    commit1

---
+++ main.py
@@ -0,0 +1,20 @@
+imie = 'Piotr'
+ilosc_ksiazek = 10
+srednia = 4.5
+
+
+
+
+print ("Hello World " + imie)
+print("Max " + str(ilosc_ksiazek))
+print("Srednia: " + str(srednia))
+
+print('ilosc_ksiazek ' + str(ilosc_ksiazek))
+
+
+samochody = int(5)
+samoloty = int(10)
+if samochody > samoloty:
+    print('there  is more cars than planes')
+else:
+    print('there is more planes than cars')
---
+++ zadanie6.py
@@ -0,0 +1,7 @@
+imie = 'Ala'
+ile = 2
+zwierze = 'kot'
+
+print("{0} ma {1}a sztuk {2}".format(imie, zwierze, ile))
+
+print('cos tam"costam" blabla')
